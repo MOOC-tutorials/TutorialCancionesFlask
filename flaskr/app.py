43a441ddc02e5b196f70229dca7bb95f32d9a51e
juanpabloreyesg <jpreyes@Leviathan.localdomain>
2021-05-21 17:47:47 -0500

    Creado esquema JSON de la clase Usuario

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,8 @@
 from flaskr import create_app
-from .modelos import db, Album, AlbumSchema, Medio
+from .modelos import db
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 db.init_app(app)
 db.create_all()
-
-with app.app_context():
-
-    a = Album(titulo="Prueba", anio=1999, descripcion="Lorem Ipsum", medio=Medio.CASETE)
-    a_schema = AlbumSchema()
-    db.session.add(a)
-    db.session.commit()
-    print([a_schema.dump(a) for c in Album.query.all()])
