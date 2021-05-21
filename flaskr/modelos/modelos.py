97db0a236d26d1b2cbba6e73286f544dd3b61316
juanpabloreyesg <jpreyes@Leviathan.localdomain>
2021-05-21 17:25:28 -0500

    Creación de la clase Album

--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
@@ -9,5 +10,14 @@ class Cancion(db.Model):
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
 
-    def __repr__(self):
-        return "{},{},{},{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+class Medio(enum.Enum):
+   DISCO = 1
+   CASETE = 2
+   CD = 3
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(512))
+    medio = db.Column(db.Enum(Medio))
