512c35bae293684277dc1a86a0c294cf34bdcc23
juanpabloreyesg <jpreyes@Leviathan.localdomain>
2021-05-21 19:49:37 -0500

    Añadida funcionalidad para crear una canción

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,5 +4,4 @@ def create_app(config_name):
     app = Flask(__name__)  
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['FLASK_RUN_PORT'] = 5001
     return app
