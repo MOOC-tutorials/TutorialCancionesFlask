6f6df819b3842bc1dd9192aa7d5dc016368d8091
juanpabloreyesg <jpreyes@Leviathan.localdomain>
2021-06-10 10:54:42 -0500

    Añadida vista para inicio  de sesión

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
 from flask_restful import Api
 from .modelos import db
-from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
+from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaLogIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
 
 app = create_app('default')
 app_context = app.app_context()
@@ -14,6 +14,7 @@ api = Api(app)
 api.add_resource(VistaCanciones, '/canciones')
 api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
 api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
 api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
 api.add_resource(VistaAlbum, '/album/<int:id_album>')
 api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
